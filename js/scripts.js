c493f9f5ea592fe493b103b13b77b233bfb94a6b
vivoA <vivoeos890@gmail.com>
2022-03-08 07:39:16 +0200

    css drawing

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,19 @@ $(function(){
        }
    })
 })//end page
+
+
+
+
+
+//1-Creating object with a constructor
+function person(name, age, address){
+    this.name=name,
+    this.age=age,
+    this.address=address
+}
+
+const myFather=new person("hossam", 55, "cairo")
+const myMother=new person("flower", 45, "mansoura")
+
+document.getElementById("family").innerHTML=`my father is ${myFather.name} and my mother age is ${myMother.age}`
